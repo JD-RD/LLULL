616df03c8ab99c74a1de524aeb7f4ef8d41d521e
bernat <elbernat@gmail.com>
2019-02-24 18:40:46 -0500

    Scene4js started Scene5 Perlin Noise Experiment x3.value()

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ function setup() {
     manager.addScene ( Scene9 );
     // SCENE1 STARTS HERE
     // manager.showScene( Intro );
-    manager.showScene( Scene3 );  
+    manager.showScene( Scene4 );  
     // canvas
     createCanvas(w, h);
     background(0);
@@ -76,10 +76,10 @@ class Dot {
         ellipse(this.x, this.y, s, s)
     }
 
-    creationBlubby() {
+    creationBlubby(color) {
         beginShape()
         noStroke()
-        fill('yellow')
+        fill(color)
         translate(w/2, h/2)
         for(let a = 0; a < TWO_PI; a += 0.02){
             let xoff = map(cos(a), -1, 1, 0, noiseMax)// offset through my perlinNoise spectrum in x

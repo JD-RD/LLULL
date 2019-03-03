ad9d91ccdc06cdc0548dce9cc0fc7779d40e4ad8
JD <jd@lafaceb.live>
2019-03-03 18:37:11 -0500

    scene-1 re-fix

--- src/js/scene-1.js
+++ src/js/scene-1.js
@@ -13,23 +13,23 @@ function Scene1 () {
 
   this.setup = function () {
     // dots at 0,0 creation
-    dot1 = new this.Dot(0, 0)
-    dot2 = new this.Dot(0, 0)
+    dot1 = new Dot(0, 0)
+    dot2 = new Dot(0, 0)
   }
 
   this.draw = function () {
     // bg - x2 adds x6 substracts alpha
-    this.background(255, 199, 26, this.x2.value() + 5 - this.x6.value() * 1.25)
+    background(255, 199, 26, x2.value() + 5 - x6.value() * 1.25)
     // grid translations - x3 adds x7 substracts rotation
-    this.translate(this.w / 2 + this.x3.value() - this.x7.value(), this.h / 2)
-    this.rotate(this.radians(this.frameCount / 10 + this.x3.value() - this.x7.value()))
+    translate(w / 2 + x3.value() - x7.value(), h / 2)
+    rotate(radians(frameCount / 10 + x3.value() - x7.value()))
     // dot1
-    dot1.creation(dotSize + this.x0.value() * 2, colors[1])
-    dot1.randomness1(this.x1.value() / 10)
+    dot1.creation(dotSize + x0.value() * 2, colors[1])
+    dot1.randomness1(x1.value() / 10)
     dot1.checkDistance(dotSize / 2)
     // dot2
-    dot2.creation(dotSize / 2 + this.x4.value() * 2, colors[2])
-    dot2.randomness1(this.x5.value() / 10)
+    dot2.creation(dotSize / 2 + x4.value() * 2, colors[2])
+    dot2.randomness1(x5.value() / 10)
     dot2.checkDistance(dotSize)
     dot2.checkCollision(dot1, dotSize)
   }
